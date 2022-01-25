85bcc776a282c48a9de461ee4fccad912369d335
root <root@S8-P0.localdomain>
2022-01-25 21:08:31 +0000

    adding s3 terraform

---
+++ backend/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+	region = "eu-west-2"
+	shared_credentials_file = "~/.aws/credentials"
+	profile = "default"
+}
+
+resource "aws_s3_bucket" "steven-website-01" {
+	bucket = "steven-website-01"
+	acl = "public-read"
+	policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [{
+    "Sid": "AllowPublicRead",
+    "Effect": "Allow",
+    "Principal": {
+      "AWS": "*"
+    },
+    "Action": "s3:GetObject",
+    "Resource": "arn:aws:s3:::steven-website-01/*"
+  }]
+}
+EOF
+}
