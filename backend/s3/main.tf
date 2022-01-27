5c32806cb36e5919af63f004b204823f219821c9
root <root@S8-P0.localdomain>
2022-01-27 21:47:47 +0000

    added ssl and dns for sofcloud.link

--- backend/s3/main.tf
+++ backend/s3/main.tf
@@ -22,15 +22,18 @@ resource "aws_s3_bucket" "steven-website-01" {
   }]
 }
 EOF
+	website {
+	  index_document = "index.html"
+}
+
 }
  resource "aws_cloudfront_distribution" "steven-website-01" {
 	enabled = true
 	is_ipv6_enabled = true
 	default_root_object = "index.html"
-
 	origin {
 	  domain_name = "${aws_s3_bucket.steven-website-01.bucket_domain_name}"
-	  origin_id = "stevens3"
+	  origin_id = "sofcloud.link"
 	}
 	restrictions {
 	  geo_restriction {
@@ -39,7 +42,7 @@ EOF
 	}
    }	
 	default_cache_behavior {
-	  target_origin_id = "stevens3"
+	  target_origin_id = "sofcloud.link"
 
 	  allowed_methods = ["GET","HEAD"]
 	  cached_methods = ["GET", "HEAD"]
@@ -57,9 +60,29 @@ EOF
 	default_ttl = 7200
 	max_ttl = 86400
 	}
-	
+
+	aliases = ["sofcloud.link"]
+
 	viewer_certificate {
-	cloudfront_default_certificate = true
+	acm_certificate_arn = "arn:aws:acm:us-east-1:466384468619:certificate/7021e82f-4991-4418-a385-686731d03ff4"
+	ssl_support_method = "sni-only"
 	}
-  }
+}
+
+data "aws_route53_zone" "main-a-record" {
+	name = "sofcloud.link"
+}
+
+resource "aws_route53_record" "sofcloud" {
+	zone_id = "${data.aws_route53_zone.main-a-record.zone_id}"
+	name = "sofcloud.link"
+	type = "A"
+
+	alias {
+	name ="${aws_cloudfront_distribution.steven-website-01.domain_name}"
+	zone_id ="${aws_cloudfront_distribution.steven-website-01.hosted_zone_id}"
+	evaluate_target_health = false
+	}
+}
+	
 
